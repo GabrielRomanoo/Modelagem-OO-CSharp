42cc98e08db336dcb26d7c0f31125f65da6a5419
gabriel <gabriel.romano.3194@hotmail.com>
2021-04-08 08:36:51 -0300

    primeiro commit

---
+++ ByteBank/ByteBank/Funcionarios/Diretor.cs
@@ -0,0 +1,28 @@
+﻿using ByteBank.Sistemas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Diretor : FuncionarioAutenticavel
+    {
+
+        public Diretor(string cpf) : base (5000,cpf)  //indica que deve chamar automaticamente o construtor da classe base (super) antes do construtor do diretor, passando o cpf pra frente
+        {
+            //A ordem de execução é sempre primeiro o construtor da classe base e depois o construtor da classe derivada.
+        }
+        public override double GetBonificacao()
+        {
+            //return base.Salario + base.GetBonificacao(); //base seria o super no java, pega o elemento da classe pai
+            return base.Salario * 0.5; 
+        }
+
+        public override void AumentarSalario()
+        {
+            Salario *= 1.15;
+        }
+    }
+}
---
+++ ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public abstract class Funcionario
+    {
+        public static int TotalDeFuncionarios { get; private set; }
+
+        public string Nome { get; set; }
+        public string CPF { get; private set; }
+        public double Salario { get; protected set; }
+
+        public Funcionario(double salario, string cpf)
+        {
+            CPF = cpf;
+            Salario = salario;
+            TotalDeFuncionarios++;
+        }
+
+        /* public virtual double GetBonificacao() //virtual indica que esse metodo pode ser sobreescrito
+        {
+            return Salario * 0.10;
+        } */
+
+        public abstract double GetBonificacao(); //o abstract indica que esse metodo DEVE ser sobreescrito
+
+        public abstract void AumentarSalario();
+    }
+}
